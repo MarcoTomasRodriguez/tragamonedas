0690ef84d1efd01ba0936fd4f4fd92372c95fb17
Marco Tomas Rodriguez <marcotomasrodriguez@gmail.com>
2021-09-25 14:34:43 -0300

    Release v1.0.0

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 
 Este programa es una simulación de una máquina tragamonedas, cuyos tiros se almacenarán para un posterior análisis de datos.
 
+## Índice
+
+- [Máquina Tragamonedas](#máquina-tragamonedas)
+  - [Índice](#índice)
+  - [Especificación](#especificación)
+  - [Instalación](#instalación)
+    - [Requisitos](#requisitos)
+    - [Pasos](#pasos)
+  - [Manual de Uso](#manual-de-uso)
+    - [Leer datos recolectados](#leer-datos-recolectados)
+
 ## Especificación
 
 Se deberá crear un programa en Python que simule el comportamiento de una máquina tragamonedas con 3 slots, y valores aleatorios entre 1 y 6.
 Al iniciar el programa, se deberán mostrar todos los slots en 0, con el texto `¡Haga su primer tiro!` en color azul claro, y un boton con el texto `Tirar`.
-Al hacer click en tirar, se deben generar numeros aleatorios del 1 al 6 en cada slot, si todos coinciden el usuario ganó y se deberá mostrar el texto `¡Usted ganó!` en color verde claro; en su defecto, deberá mostrarse el texto `¡Siga participando!` en color azul claro.
+El usuario podrá visualizar un sistema de apuesta, con el cual deberá interactuar.
+En primer lugar, contaremos con una casilla donde el usuario indicará el monto a ingresar, para así luego introducirlo presionando el botón `depositar`.
+Una vez depositado, el usuario será capaz de ver la cantidad de dinero disponible en el apartado de `balance`. A la hora de apostar, abrá otro casillero donde el usuario deberá ingresar un monto con el cual poder apostar.
+Este tendrá que ser mayor que 0 y que no supere lo disponible en el balance.
+Al hacer click en tirar, se deben generar numeros aleatorios del 1 al 6 en cada slot. En caso de que todos coincidan, se mostrará el texto `¡Usted ganó!` en color verde claro; El usuario será el ganador, por ende, será recompensado con un multiplicador de x5 hacia la apuesta anteriormente introducida. En su defecto, deberá mostrarse el texto `¡Siga participando!` en color azul claro, y el usuario perderá su apuesta.
 Cada tiro, deberá almacenarse en un archivo en formato [CSV](https://es.wikipedia.org/wiki/Valores_separados_por_comas), localizado en el mismo directorio que el programa y con el nombre `shoots.csv`.
 
 ## Instalación
@@ -21,9 +36,13 @@ Cada tiro, deberá almacenarse en un archivo en formato [CSV](https://es.wikiped
 
 1. Clonar el repositorio.
 2. Abrir una terminal en el directorio raíz del repositorio.
-3. Ejecutar `python3 src/tragamonedas.py` o `python src/tragamonedas.py` (en caso de tener Python 3 instalado por defecto).
+3. Ejecutar `python3 tragamonedas.py` o `python tragamonedas.py` (en caso de tener Python 3 instalado por defecto).
 
 ## Manual de Uso
+
+1. Una vez ejecutado el programa, escoja un monto a ingresar y presione el botón "depositar".
+2. Luego de haber depositado, podra empezar a jugar, eligiendo la cantidad de dinero que se pondrá a juego.
+3. En caso de que no desee continuar, el usuario podrá retirar el total de su balance dándole click a "retirar".
 
 ### Leer datos recolectados
 
--- tragamonedas.py
+++ tragamonedas.py
@@ -1,58 +1,156 @@
+import webbrowser
 from random import randint
+
 import tkinter as tk
+import tkinter.messagebox
 
-window = tk.Tk()
-window.title("Tragamonedas")
+class Application(tk.Frame):
+    # Tkinter Widgets
+    master: tk.Tk
+    balance_label: tk.Label
+    balance_var: tk.DoubleVar
+    bet_var: tk.DoubleVar
+    numbers_label: tk.Label
 
-# Initialize window title.
-title = tk.Label(text="Maquina Tragamonedas", font=("Arial", 16))
-title.pack(padx=10, pady=10)
+    # Application state
+    balance: float
+    bet: float
 
-# Initialize numbers.
-number_labels = [tk.Label(text="0") for _ in range(3)]
-for number_label in number_labels:
-    number_label.pack(padx=5, pady=5)
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.balance = 0
+        self.master = master
+        self.configure()
+        self.create_widgets()
+        self.pack()
+    
+    # Configure tkinter master.
+    def configure(self):
+        self.master.title("Tragamonedas")
 
+        menu_bar = tk.Menu(self.master)
+        help_menu = tk.Menu(self.master, tearoff=0)
+        help_menu.add_command(label="Manual de Usuario", command=self.redirect_to_help)
+        help_menu.add_command(label="Acerca de", command=self.show_about)
+        menu_bar.add_cascade(label="Ayuda", menu=help_menu)
+        self.master.config(menu=menu_bar)
 
-# Generate 3 random numbers, check if all of them are equal and log them in a csv.
-def shoot():
-    generated_numbers = []
+    # Create tkinter widgets.
+    def create_widgets(self):
+        # Initialize title label.
+        title_label = tk.Label(text="Maquina Tragamonedas", font=("Arial", 16))
+        title_label.pack(padx=10, pady=10)
 
-    for number_label in number_labels:
-        # Generate number between 1 and 6.
-        generated_number = randint(1, 6)
+        # Initialize balance label.
+        self.balance_label = tk.Label(text="Dinero: $0", font=("Arial", 12))
+        self.balance_label.pack(padx=10, pady=10)
 
-        # Append generated number to list.
-        generated_numbers.append(generated_number)
+        # Initialize balance input.
+        self.balance_var = tk.DoubleVar()
+        self.balance_input = tk.Entry(textvariable=self.balance_var)
+        self.balance_input.pack()
+
+        # Initialize balance button.
+        deposit_button = tk.Button(text="Depositar", command=self.deposit)
+        deposit_button.pack()
+
+        # Initialize bet label.
+        bet_label = tk.Label(text="Ingrese su apuesta")
+        bet_label.pack()
+
+        # Initialize bet input.
+        self.bet_var = tk.DoubleVar()
+        bet_input = tk.Entry(textvariable=self.bet_var)
+        bet_input.pack()
+
+        # Initialize numbers.
+        self.numbers_label = tk.Label(text="0   0   0", font=("Arial", 12))
+        self.numbers_label.pack(padx=5, pady=5)
+        
+        # Initialize shoot button & bind "Click" event to shoot.
+        shoot_button = tk.Button(text="Tirar", command=self.shoot)
+        shoot_button.pack()
+
+        # Initialize message label.
+        self.message = tk.Label(text="¡Haga su primer tiro!", bg="lightblue")
+        self.message.pack(padx=5, pady=5)
+
+    # Update balance & balance label.
+    def update_balance(self, new_balance: float) -> None:
+        assert new_balance >= 0
+        self.balance = new_balance
+        self.balance_label.config(text=f"Dinero: ${self.balance}")
+
+    # Show a message. This can either be with a message label (if success or info) or a message box (if error).
+    def show_message(self, text: str, type: str) -> None:
+        if type in ["success", "info"]:
+            color_dict = {"success": "lightgreen", "info": "lightblue"}
+            self.message.config(text=text, bg=color_dict[type])
+        elif type == "error":
+            tk.messagebox.showerror("Error", text)
+    
+    # Redirect to help website.
+    # Caller: help_menu on "Manual de Usuario" click.
+    def redirect_to_help(self) -> None:
+        webbrowser.open("https://github.com/MarcoTomasRodriguez/tragamonedas")
+
+    # Show about.
+    # Caller: help_menu on "Acerca de" click.
+    def show_about(self) -> None:
+        tk.messagebox.showinfo("Acerca de", "Trabajo realizado por Alejo Scarlato y Marco Rodriguez")
+
+    # Deposit a balance.
+    # Caller: deposit_button on click.
+    def deposit(self) -> None:
+        # Get deposited balance.
+        deposited_balance = self.balance_var.get()
+        
+        # Check if deposited balance is lower than 0. If so, show an error message.
+        if deposited_balance < 0:
+            self.show_message("No puede depositar un valor inferior a 0.", "error")
+            return
+
+        # Update balance & set deposited balance to 0.
+        self.update_balance(self.balance + deposited_balance)
+        self.balance_var.set(0)
+
+    # Generate 3 random numbers, check if all of them are equal and log them in a csv.
+    # Caller: shoot_button on click.
+    def shoot(self):
+        # Get current bet.
+        bet = self.bet_var.get()
+
+        # Check if bet is lower than 0.
+        if bet < 0:
+            self.show_message("La apuesta no puede ser menor a 0.", "error")
+            return
+
+        # Check if bet is greater than balance, if so, show an error message.
+        if bet > self.balance:
+            self.show_message("Balance insuficiente.", "error")
+            return
+
+        # Generate 3 numbers between 1 and 6.
+        generated_numbers = [randint(1, 6) for _ in range(3)]
 
         # Set label text to the generated number.
-        number_label["text"] = generated_number
+        self.numbers_label.config(text="   ".join(map(str, generated_numbers)))
 
-    # Check whether the user won or not, and update the message label.
-    # All the generated numbers must be equal in order for the user to win.
-    if all(x == generated_numbers[0] for x in generated_numbers):
-        message.config(text="¡Usted gano!", bg="lightgreen")
-    else:
-        message.config(text="¡Siga participando!", bg="lightblue")
+        # Append shoot to shoots.csv.
+        with open('shoots.csv', mode='a') as shoots_file:
+            shoots_file.write(",".join(map(str, generated_numbers)))
+            shoots_file.write("\n")
 
-    # Append shoot to shoots.csv.
-    with open('shoots.csv', mode='a') as shoots_file:
-        shoots_file.write(",".join(map(str, generated_numbers)))
-        shoots_file.write("\n")
+        # Check whether the user won or not, and update the message label.
+        # All the generated numbers must be equal in order for the user to win.
+        if all(x == generated_numbers[0] for x in generated_numbers):
+            self.update_balance(self.balance + bet * 10)
+            self.show_message("¡Usted gano!", "success")
+        else:
+            self.update_balance(self.balance - bet)
+            self.show_message("¡Siga participando!", "info")
 
 
-# Initialize shoot button & bind "Click" event to shoot.
-shoot_button = tk.Button(text="Tirar", command=shoot)
-shoot_button.pack()
-
-# Initialize message label.
-message = tk.Label(text="¡Haga su primer tiro!", bg="lightblue")
-message.pack(padx=5, pady=5)
-
-# Show program authors.
-authors = tk.Label(
-    text="Trabajo realizado por Alejo Scarlato y Marco Rodriguez")
-authors.pack(padx=5, pady=5)
-
-# Enter window main loop and prevent it from closing.
-window.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
