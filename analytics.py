af432abfd4eef9b8ccb627cccf568c8e287a06a2
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2021-10-22 23:22:29 -0300

    Analytics assignment.

---
+++ analytics.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('shoots.csv', header=None)
+numbers = df.to_numpy().flatten()
+
+unique, counts = np.unique(numbers, return_counts=True)
+
+fig, ax = plt.subplots()
+ax.pie(counts, labels=unique, autopct='%1.1f%%', shadow=True, startangle=90)
+ax.axis('equal')
+plt.show()
